d02a138deb0fcbe8d97afc9a369c7b672bd441af
GitOSC9 <105081942+GitOSC9@users.noreply.github.com>
2022-06-15 14:11:54 -0500

    Release Ultracore 3S

---
+++ Ultracore3S.bat
@@ -0,0 +1,16 @@
+@echo off
+color 09
+
+:boot
+cls
+echo Welcome to Ultracore 3S. Type !start to start or run an alternative command.
+set /p commandboot1=Enter command... 
+if %commandboot1%==!start goto :ultracore-panel
+cls
+
+:ultracore-panel
+cls
+echo Select a version to test.
+echo -- No versions of Base OS have been made yet. When one is available, you can test it here. Type !back -boot to return to boot or run an alternative command. --
+set /p commanducp1=Enter command... 
+if %commanducp1%==!boot goto :boot
