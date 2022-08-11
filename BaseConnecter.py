35b810fd70c0879bda66aad5a3004b46485d6c1d
SpanTvggg <106777513+SpanTvggg@users.noreply.github.com>
2022-08-11 13:28:44 -0500

    One slight change

--- BaseConnecter.py
+++ BaseConnecter.py
@@ -1,6 +1,6 @@
 print("Hello, World!")
 print("Press the letter (t) togo back ")
-Answer = input("Type here     ")
+Answer = input("Type here   ")
 if Answer == "t":
     import subprocess
     subprocess.call(['BaseUI.bat'])
